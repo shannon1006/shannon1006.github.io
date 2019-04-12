769e9da64f0088da97df14ef4c967c16383b13f9
shuyangwang <shuyangwang@paypal.com>
2019-04-12 15:17:34 -0700

    run build

---
+++ precache-manifest.21f4342fe72aeae2a5cbf8faf496e459.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "7dfde6a4ed9e4033b0ae",
+    "url": "/static/css/main.22aa7961.chunk.css"
+  },
+  {
+    "revision": "7dfde6a4ed9e4033b0ae",
+    "url": "/static/js/main.0c87e50b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "187651429ef65ffd3f53",
+    "url": "/static/js/2.3e19b669.chunk.js"
+  },
+  {
+    "revision": "29ed731333b8fee76d7bc443b5f0c8fd",
+    "url": "/static/media/IMG_4543.29ed7313.jpg"
+  },
+  {
+    "revision": "a1559cc73d6e1544be216de544948cf1",
+    "url": "/static/media/IMG_4144.a1559cc7.jpg"
+  },
+  {
+    "revision": "b09d7ea526b0922f58c5ebd23688c10e",
+    "url": "/static/media/IMG_3450.b09d7ea5.jpg"
+  },
+  {
+    "revision": "17bd3f3bad0c4e4e87d03328979bc33c",
+    "url": "/static/media/IMG_5278.17bd3f3b.JPG"
+  },
+  {
+    "revision": "1a30349be0bb3d141a09bde9827924b5",
+    "url": "/static/media/DSC_0446.1a30349b.JPG"
+  },
+  {
+    "revision": "1932c1e6b7533f5eb51f065995a6324d",
+    "url": "/static/media/DSC_0451.1932c1e6.JPG"
+  },
+  {
+    "revision": "12300c3c86fde36c0fc53df68ff85d67",
+    "url": "/static/media/background.12300c3c.jpg"
+  },
+  {
+    "revision": "f047630eb3c6de68e78c68ff35cffc44",
+    "url": "/index.html"
+  }
+];
